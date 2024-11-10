6323bc2dd2d7c9bb51ab6a44d69244401712fd2d
cocobabb <cocobab05108@gmail.com>
2024-11-11 00:29:20 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -8,12 +8,17 @@ for _ in range(9):
 h_total = sum(dwarfs)
 fake = []
 real_dwarfs =[]
+out_for = False
 for i in range(8):
   for j in range(i+1, 9):
     if h_total - dwarfs[i] - dwarfs[j] == 100:
       fake = [dwarfs[i], dwarfs[j]]
+      out_for = True
       break
-  real_dwarfs = [el for el in dwarfs if el not in fake]
-  real_dwarfs.sort()
+  if out_for:
+    break
+real_dwarfs = [el for el in dwarfs if el not in fake]
+real_dwarfs.sort()
+
 for real in real_dwarfs:
   print(real)
