3fe53c7a9893478a41c6736f238fdc06a162f0ba
cocobabb <cocobab05108@gmail.com>
2025-04-20 23:18:13 +0900

    [level 1] Title: 폰켓몬, Time: 0.82 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,13 @@
+def solution(nums):
+    answer = 0
+    count_choice = int(len(nums)/2)
+
+    category = set(nums)
+    count_category = len(category)
+
+    if count_choice > count_category:
+        answer = count_category
+    else:
+        answer = count_choice
+    
+    return answer
